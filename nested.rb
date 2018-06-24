3a13bfa867e6db696b3c162366c199bcc434d45e
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 04:24:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,3 +126,6 @@ def adding_to_dennis
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 return programmer_hash
 end
+
+adding_to_dennis
+puts programmer_hash[:dennis_ritchie][:languages].inspect
